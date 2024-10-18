862af3e662b7771174cd2b6d2fad79a078737bda
your_email@abc.example <your_email@abc.example>
2024-10-18 13:27:52 +0300

    add: array

---
+++ array.ts
@@ -0,0 +1,10 @@
+const fryits = ['apple', 'strabery', 'banana', 'mango','ananas'] 
+const vegetays = ['potato','cucumber','cabbage','carrot','tomato']
+
+fryits.push('gorila')
+const arrau3 = fryits.concat(vegetays);
+
+arrau3.sort();
+
+console.log(arrau3);
+
---
+++ object.ts
@@ -0,0 +1,10 @@
+const Maschinencharakterisierung = {
+    salon : 'тёмный',
+    kissen : 'Передние',
+    assistenzsysteme :'Датчик дождя',
+    außeres : 'Легкосплавные диски' ,
+    innenbereich :'Люк',
+    klima :'Климат-контроль' ,
+    heizung :'Сидений',
+    multimedia : ''
+}
